053c9a8f8307b85906854a9597951d278b2b3f03
kunallad54 <kunallad5473@gmail.com>
2021-06-22 14:04:48 +0530

    Can delete first element of linked list

--- LinkedList.java
+++ LinkedList.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 /**
- * Ability to insert 30 between 56 and 70
- * - Final Sequence: 56->30->70
- * Here using method insertNewNode Method it can insert any data at any position user wants 
- * in between the linked list
+ * Ability to delete the first element in the LinkedList of sequence 56->30->70 - Write pop method
+ * - Note there is new head
+ * - Final Sequence: 30->70
  * 
+ * Here it can insert,create,and can delete first element of linked list
+ *
  * @author Krunal Lad
  * @Since 22-06-2021
  */
@@ -14,27 +15,56 @@ public class LinkedList {
     static Scanner scanner = new Scanner(System.in);
     private INode head;
     private INode tail;
-    private  int size;
+    private int size;
 
     public LinkedList() {
         this.head = null;
         this.tail = null;
         this.size = 0;
     }
-    int size(){
+
+    int size() {
         return this.size;
     }
+
     public static void main(String[] args) {
-        linkedList.createLinkedList();
-        linkedList.printLinkedList();
-        
-        System.out.println("Enter the data you want to insert");
-        Integer element = scanner.nextInt();
-        System.out.println("Enter the position where you want to insert the data ");
-        int position = scanner.nextInt();
-        
-        linkedList.insertNewNode(element,position);
-        linkedList.printLinkedList();
+
+        boolean flag = true;
+        while (flag) {
+            int choice = linkedList.userConsole();
+            switch (choice) {
+                case 1:
+                    linkedList.createLinkedList();
+                    break;
+                case 2:
+                    System.out.println("Enter the data you want to insert");
+                    Integer element = scanner.nextInt();
+                    System.out.println("Enter the position where you want to insert the data ");
+                    int position = scanner.nextInt();
+                    linkedList.insertNewNode(element, position);
+                    break;
+                case 3:
+                    linkedList.popFirst();
+                    break;
+                case 4:
+                    linkedList.printLinkedList();
+                    break;
+                case 5:
+                    flag = false;
+                    break;
+            }
+        }
+    }
+
+    // user console
+    public int userConsole() {
+        System.out.println("Press 1 to create Linked List");
+        System.out.println("Press 2 to insert element in  Linked List");
+        System.out.println("Press 3 to delete first element of Linked List");
+        System.out.println("Press 4 to display Linked List");
+        System.out.println("Press 5 to EXIT ");
+
+        return scanner.nextInt();
     }
 
     // creates linked list
@@ -48,10 +78,10 @@ public class LinkedList {
             size++;
             System.out.println("Want to add more elements ? Press 1 \n Press 0 to exit");
             int userChoice = scanner.nextInt();
-            if(userChoice == 0)
+            if (userChoice == 0)
                 flag = false;
         }
-        System.out.println("Size is : "+size);
+        System.out.println("Size is : " + size);
     }
 
     // adds new nodes to linked list
@@ -71,11 +101,11 @@ public class LinkedList {
     }
 
     // appends new nodes to linked list
-    private void append(INode newNode){
+    private void append(INode newNode) {
         // checks if linked list is empty then head and tail points to first node
-        if(head == null)
+        if (head == null)
             head = newNode;
-        if(tail == null)
+        if (tail == null)
             tail = newNode;
         else {
             //temporary node will initially store tail value,
@@ -86,24 +116,24 @@ public class LinkedList {
         }
     }
 
-    // inserting new node at particular position user wants 
-    public void insertNewNode(Integer element, int position){
-        
+    // inserting new node at particular position user wants
+    public void insertNewNode(Integer element, int position) {
+
         // to verify index value is in range
-        if(position < 0 || position> size)
+        if (position < 0 || position > size)
             System.out.println("Index Number out of range");
         else {
             Node<Integer> newNode = new Node<Integer>(element);
             // if position = 0 then adds element at the first else iterates till position - 1 and inserts element
-            if(position == 0) {
+            if (position == 0) {
                 newNode.setNext(head);
                 head.setNext(newNode);
             } else {
                 INode currentNode = head;
                 INode previousNode = null;
                 int i = 0;
-                // iterates till position - 1 
-                while (i < position - 1){
+                // iterates till position - 1
+                while (i < position - 1) {
                     previousNode = currentNode;
                     currentNode = currentNode.getNext();
                     i++;
@@ -111,6 +141,14 @@ public class LinkedList {
                 newNode.setNext(currentNode);
                 previousNode.setNext(newNode);
             }
+        }
+    }
+
+    public void popFirst() {
+        if (head == null) {
+            System.out.println("No element to delete");
+        } else {
+            head = head.getNext();
         }
     }
 
