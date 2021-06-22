9c22a29f788c784fa2d6de1aa0f79c7dec751962
kunallad54 <kunallad5473@gmail.com>
2021-06-22 19:40:09 +0530

    Stack push,pop,peak operations performed

--- Stack.java
+++ Stack.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 /**
- * Ability to create a Stack of 56->30->70
+ * Ability to peak and pop from the Stack till it is empty 56->30->70
  * - Use LinkedList to do the Stack Operations
- * - Here push will internally call add method on LinkedList.
- * - So 70 will be added first then 30 and then 56 to make 56 on top of the Stack
+ * Here Stack can perform Push,pop,peak operations 
  *
  * @author Krunal Lad
  * @Since 22-06-2021
@@ -13,6 +12,7 @@ import java.util.Scanner;
 //@param <K>
 public class Stack<K> {
     private final LinkedList linkedList;
+    static Scanner scanner = new Scanner(System.in);
 
     public Stack() {
         linkedList = new LinkedList();
@@ -24,6 +24,17 @@ public class Stack<K> {
         linkedList.add(newNode);
     }
 
+    //peak method returns top of stack
+    public void peak(){
+        Integer head = (Integer) linkedList.head();
+        System.out.println(head);
+    }
+
+    // deletes top element of stack
+    public void pop(){
+        linkedList.popFirst();
+    }
+
     // printing stack values
     public void printStack() {
         linkedList.printLinkedList();
@@ -32,17 +43,41 @@ public class Stack<K> {
     // main method
     public static void main(String[] args) {
         Stack stack = new Stack();
-        Scanner scanner = new Scanner(System.in);
+
         boolean flag = true;
         while (flag) {
-            System.out.println("Enter the elements that you want to push in Stack : ");
-            Integer element = scanner.nextInt();
-            stack.push(element);
-            System.out.println("Press 0 to exit to continue press any key ");
-            int userChoice = scanner.nextInt();
-            if (userChoice == 0)
-                flag = false;
+            int choice = stack.userConsole();
+            switch (choice){
+                case 1:
+                    System.out.println("Enter the elements that you want to push in Stack : ");
+                    Integer element = scanner.nextInt();
+                    stack.push(element);
+                    break;
+                case 2:
+                    stack.peak();
+                    //System.out.println("Element at the top of stack is : "+peak);
+                    break;
+                case 3:
+                    stack.pop();
+                    System.out.println("Successfully removed top element of stack !!!");
+                    break;
+                case 4:
+                    stack.printStack();
+                    break;
+                case 5:
+                    flag = false;
+                    break;
+            }
         }
-        stack.printStack();
+    }
+
+    public int userConsole(){
+        System.out.println("Press 1 - to Push in stack");
+        System.out.println("Press 2 - to Peak in stack");
+        System.out.println("Press 3 - to Pop in stack");
+        System.out.println("Press 4 - to Display stack elements");
+        System.out.println("Press 5 - to EXIT ");
+
+        return scanner.nextInt();
     }
 }
